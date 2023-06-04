d531883bb0768dc6a98cfd137c35a3d95d1fa9ba
JuiceBox7 <juiceinbox7@gmail.com>
2023-06-03 23:53:29 -0700

    made template

---
+++ src/template.js
@@ -0,0 +1,60 @@
+class Template extends GameScene {
+    constructor() {
+        super('template', 'Template')
+    }
+
+    preload() {
+        this.load.path = '/assets/' // <- for local
+        // this.load.path = '/CMPM-Final-Project/assets/' // <- for github
+        this.load.image('lighton', 'Buttons/Light switch on.png')
+        this.load.image('lightoff', 'Buttons/Light switch off.png')
+        this.load.audio('switchon', 'sounds/LIGHT SWITCH ON SOUND.mp3')
+        this.load.audio('switchoff', 'sounds/LIGHT SWITCH OFF SOUND.mp3')
+    }
+
+    onEnter() {
+        this.add.text(game.config.width/2, game.config.height/2, 'click here to go to test 2')
+            .setFontSize(50)
+            .setOrigin(0.5)
+            .setInteractive({useHandCursor: true})
+            .on('pointerdown', () => this.gotoScene('test2'))
+
+        this.lightOn = this.add.image(this.w-4*this.s, this.h-6*this.s, 'lighton')
+            .setScale(0.1)
+
+        this.lightOff = this.add.image(this.w-2*this.s, this.h-6*this.s, 'lightoff')
+            .setScale(0.1)
+
+        this.switchOn = this.sound.add('switchon').setVolume(0.25)
+        this.switchOff = this.sound.add('switchoff').setVolume(0.25)
+
+        if(this.screenTint.setVisible(false)) {
+            this.lightOff.setVisible(false)
+        }
+        else {
+            this.lightOn.setVisible(false)
+        }
+
+        this.light = 1
+
+        this.lightSwitchinter = this.add.text(this.w-3*this.s, this.h-8*this.s, '  \n  ')
+            .setFontSize(`${(2 * this.s) - 10}px`)
+            .setInteractive({useHandCursor: true})
+            .on('pointerover', () => this.showMessage('Toggle light switch?'))
+            .on('pointerdown', () => {
+                if (this.light == 1) {
+                    this.switchOff.play()
+                    this.lightOn.setVisible(false)
+                    this.lightOff.setVisible(true)
+                    this.light = 0
+                    this.screenTint.setVisible(true)
+                } else {
+                    this.switchOn.play()
+                    this.lightOff.setVisible(false)
+                    this.lightOn.setVisible(true)
+                    this.light = 1
+                    this.screenTint.setVisible(false)
+                }
+            });
+    }
+}
