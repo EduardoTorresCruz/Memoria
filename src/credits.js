5287b9411025b4e2b18bf3efc98b5bc359f2f950
JuiceBox7 <juiceinbox7@gmail.com>
2023-06-05 15:00:52 -0700

    added intro/outro

--- src/credits.js
+++ src/credits.js
@@ -4,6 +4,7 @@ class Credits extends Phaser.Scene {
     }
 
     create() {
+        this.cameras.main.fadeIn(1000);
         this.add.text(game.config.width/2, game.config.height/2, 'Thank You For Playing!\n\n       Credits:\n     Julian Lara\n Elyzza Joyce Marquez\n      Xing Zhong\n Eduardo Torres Cruz')
             .setFontSize(100)
             .setOrigin(0.5)
