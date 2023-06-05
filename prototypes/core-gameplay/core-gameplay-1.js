5a48706a79094a914193fd3291d4f6d70c2d9139
KingZ060 <103080501+KingZ060@users.noreply.github.com>
2023-06-05 02:34:36 -0700

    Test

--- prototypes/core-gameplay/core-gameplay-1.js
+++ prototypes/core-gameplay/core-gameplay-1.js
@@ -1,94 +1,94 @@
-class Player extends Phaser.GameObjects.Sprite {
-    constructor (scene, x, y)
-    {
-        super(scene, x, y, 'player');
+// class Player extends Phaser.GameObjects.Sprite {
+//     constructor (scene, x, y)
+//     {
+//         super(scene, x, y, 'player');
 
-        scene.add.existing(this);
-        scene.physics.add.existing(this);
+//         scene.add.existing(this);
+//         scene.physics.add.existing(this);
 
-        this.setCircle(14, 3, 6);
-        this.setCollideWorldBounds(true);
+//         this.setCircle(14, 3, 6);
+//         this.setCollideWorldBounds(true);
 
-        this.isAlive = false;
+//         this.isAlive = false;
 
-        this.speed = 280;
-        //this.target = new Phaser.Math.Vector2();
-    }
+//         this.speed = 280;
+//         //this.target = new Phaser.Math.Vector2();
+//     }
 
-    start ()
-    {
-        this.isAlive = true;
+//     start ()
+//     {
+//         this.isAlive = true;
 
-        this.scene.input.on('pointermove', (pointer) =>
-        {
-            if (this.isAlive)
-            {
-                this.target.x = pointer.x;
-                this.target.y = pointer.y;
+//         this.scene.input.on('pointermove', (pointer) =>
+//         {
+//             if (this.isAlive)
+//             {
+//                 this.target.x = pointer.x;
+//                 this.target.y = pointer.y;
                 
-                //  Add 90 degrees because the sprite is drawn facing up
-                this.rotation = this.scene.physics.moveToObject(this, this.target, this.speed) + 1.5707963267948966;
-            }
-        });
-    }
+//                 //  Add 90 degrees because the sprite is drawn facing up
+//                 this.rotation = this.scene.physics.moveToObject(this, this.target, this.speed) + 1.5707963267948966;
+//             }
+//         });
+//     }
 
-    kill ()
-    {
-        this.isAlive = false;
+//     kill ()
+//     {
+//         this.isAlive = false;
 
-        this.body.stop();
-    }
+//         this.body.stop();
+//     }
 
-    preUpdate ()
-    {
-        if (this.body.speed > 0 && this.isAlive)
-        {
-            if (Phaser.Math.Distance.Between(this.x, this.y, this.target.x, this.target.y) < 6)
-            {
-                this.body.reset(this.target.x, this.target.y);
-            }
-        }
-    }
-}
+//     preUpdate ()
+//     {
+//         if (this.body.speed > 0 && this.isAlive)
+//         {
+//             if (Phaser.Math.Distance.Between(this.x, this.y, this.target.x, this.target.y) < 6)
+//             {
+//                 this.body.reset(this.target.x, this.target.y);
+//             }
+//         }
+//     }
+// }
 
-class MainGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('MainGame');
-        this.player;
-    }
+// class MainGame extends Phaser.Scene
+// {
+//     constructor ()
+//     {
+//         super('MainGame');
+//         this.player;
+//     }
 
-    preload()
-    {
-        this.load.image("player", "./test.png");
-    }
+//     preload()
+//     {
+//         this.load.image("player", "./test.png");
+//     }
 
-    create ()
-    {
-        this.graphics = this.add.graphics();
+//     create ()
+//     {
+//         this.graphics = this.add.graphics();
 
-        this.graphics.fillStyle(0xFF00FF, 1); // color, opacity
-        let bg = this.graphics.fillRect(0, 0, 900, 900);
-        bg.depth = 1;
-        this.player = new Player(this, 400, 400);
-        //this.player.fixedToCamera = true;
-        this.player.start();
-    }
-}
+//         this.graphics.fillStyle(0xFF00FF, 1); // color, opacity
+//         let bg = this.graphics.fillRect(0, 0, 900, 900);
+//         bg.depth = 1;
+//         this.player = new Player(this, 400, 400);
+//         //this.player.fixedToCamera = true;
+//         this.player.start();
+//     }
+// }
 
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: '#000000',
-    scene: [MainGame],
-    physics: {
-        default: 'arcade',
-        arcade: { debug: false }
-    }
-};
+// const config = {
+//     type: Phaser.AUTO,
+//     width: 800,
+//     height: 600,
+//     backgroundColor: '#000000',
+//     scene: [MainGame],
+//     physics: {
+//         default: 'arcade',
+//         arcade: { debug: false }
+//     }
+// };
 
-let game = new Phaser.Game(config);
+// let game = new Phaser.Game(config);
 
-// Make sure to run live server in html file dummy
+// // Make sure to run live server in html file dummy
--- prototypes/core-gameplay/script_movement.js
+++ prototypes/core-gameplay/script_movement.js
@@ -1,66 +1,66 @@
 
-class Test1 extends Phaser.Scene {
-    constructor() {
-        super('Test1')
-    }
+// class Test1 extends Phaser.Scene {
+//     constructor() {
+//         super('Test1')
+//     }
 
-    preload() {
-        this.load.path = '../../assets/'
-        this.load.image('player', 'Delilah.png')
-    }
+//     preload() {
+//         this.load.path = '../../assets/'
+//         this.load.image('player', 'Delilah.png')
+//     }
 
-    create() {
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.player = new Player(this, 100, 100);
-        this.input.on('pointerdown', this.player.movePlayer, this.player);
-        let text = this.add.text(800, 800, 'Tap Me', { fill: '#0f0' });
-        text.setInteractive();
-        text.on('pointerdown', () => { 
-            this.scene.start('Test2'); 
-        });
+//     create() {
+//         this.cursors = this.input.keyboard.createCursorKeys();
+//         this.player = new Player(this, 100, 100);
+//         this.input.on('pointerdown', this.player.movePlayer, this.player);
+//         let text = this.add.text(800, 800, 'Tap Me', { fill: '#0f0' });
+//         text.setInteractive();
+//         text.on('pointerdown', () => { 
+//             this.scene.start('Test2'); 
+//         });
         
-    }
+//     }
 
-    update() {
-        this.player.update(this.cursors);
-    }
-}
+//     update() {
+//         this.player.update(this.cursors);
+//     }
+// }
 
-class Test2 extends Phaser.Scene {
-    constructor() {
-        super('Test2')
-    }
+// class Test2 extends Phaser.Scene {
+//     constructor() {
+//         super('Test2')
+//     }
 
-    preload() {
-        this.load.path = '../../assets/'
-        this.load.image('player', 'Delilah.png')
-    }
+//     preload() {
+//         this.load.path = '../../assets/'
+//         this.load.image('player', 'Delilah.png')
+//     }
 
-    create() {
-        let text = this.add.text(800, 800, 'Tap Me', { fill: '#0f0' });
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.player = new Player(this, 100, 100);
-        this.input.on('pointerdown', this.player.movePlayer, this.player);
-    }
+//     create() {
+//         let text = this.add.text(800, 800, 'Tap Me', { fill: '#0f0' });
+//         this.cursors = this.input.keyboard.createCursorKeys();
+//         this.player = new Player(this, 100, 100);
+//         this.input.on('pointerdown', this.player.movePlayer, this.player);
+//     }
     
-    update() {
-        this.player.update(this.cursors);
-    }
-}
+//     update() {
+//         this.player.update(this.cursors);
+//     }
+// }
 
 
-const game = new Phaser.Game({
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1900,
-        height: 1000
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {debug: true}
-    },
-    type: Phaser.AUTO,
-    scene: [Test1,Test2],
-    title: "Final Project",
-});
+// const game = new Phaser.Game({
+//     scale: {
+//         mode: Phaser.Scale.FIT,
+//         autoCenter: Phaser.Scale.CENTER_BOTH,
+//         width: 1900,
+//         height: 1000
+//     },
+//     physics: {
+//         default: 'arcade',
+//         arcade: {debug: true}
+//     },
+//     type: Phaser.AUTO,
+//     scene: [Test1,Test2],
+//     title: "Final Project",
+// });
